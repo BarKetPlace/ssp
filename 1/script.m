19ec2927b0c1bc7f145124158a5db357d944faf1
Feliszt <fcote@kth.se>
2016-01-28 15:31:16 +0100

    Added files

---
+++ 1/script.m
@@ -0,0 +1,28 @@
+%% Load stuff
+clear all
+close all
+clc
+
+load assignment1.mat
+female = audioread('female44.wav');     % female sound
+male = audioread('male44.wav');         % male sound
+Fs = 8000 ;                             % sample frequency of signal
+Fss = 44100 ;                           % sample frequency of speech
+
+%% 2- Bandwith of Speech
+close all
+clc
+
+for Fc=100:50:500
+        % set cut off frequency
+%     Fc = 100 ;          % Hz
+    Fc = Fc / Fs ;      % normalized cut off frequency
+
+        % filter the signal
+    filtered_female = lowpass(female, Fc) ;
+
+        % listen to the signal
+    soundsc(filtered_female, Fss) ;
+
+   pause(3)
+end
