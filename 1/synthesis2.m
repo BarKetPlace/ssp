2505d798acc282c390635397b1d6089b9c9e8426
Antoine Honore <antoinehonor@gmail.com>
2016-02-23 11:26:15 +0100

    noise excitation

--- 1/synthesis2.m
+++ 1/synthesis2.m
@@ -7,14 +7,14 @@ cp = 100; % Constant pitch period in samples
 pexc = zeros(n_frames*ulen, 1);
 pexc(1:cp:end) = 1;
 % Create noise excitation:
-nexc = 0   ;
+nexc = 2*rand(n_frames*ulen,1) - 1;
 n1 = 1;
 n2 = ulen;
 Z = [];
 s = zeros(n_frames*ulen,1);
 for n=1:n_frames
     % Filter the excitation through the production (vocal tract) filter:
-    [s(n1:n2), Z] = varifilter(1, A(n,:), pexc(n1:n2), Z);
+    [s(n1:n2), Z] = varifilter(1, A(n,:), nexc(n1:n2), Z);
     
     Es(n) = 1/(n2-n1+1)*sum(s(n1:n2).^2);
     %To normalize we resolve sum((s(n1:n2)*corr).^2) = E(n)
